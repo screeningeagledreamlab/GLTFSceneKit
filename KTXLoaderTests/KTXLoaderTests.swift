872f8f92cd722928eaaea08a31a2b69e7e5c9e39
Jesse Armand <jesse.armand@screeningeagle.com>
2023-12-05 16:33:30 +0800

    Add KTX texture loader unit test

--- KTXLoaderTests/KTXLoaderTests.swift
+++ KTXLoaderTests/KTXLoaderTests.swift
@@ -11,20 +11,32 @@ import XCTest
 
 class KTXLoaderTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testLoadTextureData() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        guard let device = MTLCreateSystemDefaultDevice() else {
+            XCTFail("Fail to create metal system default device")
+            return
+        }
+        let resourceName = "Cube_BaseColor"
+
+        guard let url = Bundle(for: KTXLoaderTests.self).url(forResource: resourceName, withExtension: "ktx2", subdirectory: "art.scnassets/Cube/") else {
+            XCTFail("\(resourceName).ktx2 is not available to load")
+            return
+        }
+
+        let data = try Data(contentsOf: url)
+
+        let loader = try KTXLoader(data: data, device: device)
+        let metalTexture = loader.loadTexture(using: device)
+        Swift.debugPrint("MLTTexture size (width: \(metalTexture.width), height: \(metalTexture.height))")
+        Swift.debugPrint("MLTTexture pixel format: \(metalTexture.pixelFormat)")
+        Swift.debugPrint("MLTTexture compression type \(metalTexture.compressionType)")
+        Swift.debugPrint("MLTTexture texture type \(metalTexture.textureType)")
+
+        XCTAssertEqual(metalTexture.pixelFormat, MTLPixelFormat.astc_4x4_srgb)
+        XCTAssertEqual(metalTexture.compressionType,  MTLTextureCompressionType.lossless)
+        XCTAssertEqual(metalTexture.textureType, MTLTextureType.type2D)
+        XCTAssertEqual(metalTexture.width, 256)
+        XCTAssertEqual(metalTexture.height, 256)
     }
 
 }
